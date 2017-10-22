70b120a07260c986ff1c7db73f0a15dddab9293a
Carmine De Rosa <carmine@s2kagency.it>
2017-10-22 13:20:16 +0200

    finish git classic slides

--- public/main.js
+++ public/main.js
@@ -6,44 +6,24 @@
     var current = 1;
     var slidesLen = $('section').length;
 
-    //window.addEventListener('keypress', onKeyPress, false);
-
     socket.on('key', navigate);
-       /* 
-    function onKeyPress(e){
-        e.preventDefault();
-        console.log("Press: ", e.key);
-        socket.emit('key', e.key);
-    }    
-    */
+
     function navigate(e){
-        console.log(e);
-        switch(e){
-            case "ArrowDown":
-                current++;
-            break;
-            case "ArrowUp":
-                current--;
-            break;
-        }  
-        
-        if(current<=0){current=1;}
-        if(current>slidesLen){
-            current=slidesLen;
-        }else{
-            console.log('LEN: ',slidesLen, current);
-            scrollTo(current);
+        if(e>slidesLen){
+            e=slidesLen;
         }
-        
+        scrollTo(e);
     }
 
     
     function scrollTo(slide){
-        console.log("Slide: ", slide, $('section:nth-child('+slide+')').offset().top);
+        var top = $('section:nth-child('+slide+') img.git').css('top');
+        console.log("Slide: ", slide, $('section:nth-child('+slide+')').offset().top, top);
+        
         $('section:nth-child('+slide+') img.git').css({'top': '80%','opacity':'0'});
         $("html, body").stop().animate({ scrollTop: $('section:nth-child('+slide+')').offset().top }, 500,
             function(){
-                $('section:nth-child('+slide+') img.git').animate({'top': '50%','opacity':'1'},1000,'easeOutExpo',function(){});
+                $('section:nth-child('+slide+') img.git').animate({'top': top,'opacity':'1'},1000,'easeOutExpo');
             }
         );
     }
